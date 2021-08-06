5b5cf7987d5f34fe498d6a0caf0a2957f7da6b2e
Onur Atakan ULUSOY <atadogan06@gmail.com>
2021-08-06 12:12:00 +0000

    Added new version of ask_me_something library

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 setup(name='ONUR_Voice_Assistant',
-version='0.1.1',
+version='0.1.2',
 description="""A modular and expandable voice assistant""",
 long_description="""
 # ONUR Voice Assistant | [![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/) [![GitHub license](https://img.shields.io/github/license/onuratakan/ONUR_Voice_Assistant)](https://github.com/onuratakan/ONUR_Voice_Assistant/blob/master/LICENSE)
@@ -35,7 +35,7 @@ packages=["ONUR_Voice_Assistant"],
 package_dir={'':'src'},
 install_requires=[
     "say-me-something==0.1.1",
-    "ask-me-something==0.1.1",
+    "ask-me-something==0.1.2",
     "selenium==3.141.0",
     "SpeechRecognition==3.8.1",
 ],
